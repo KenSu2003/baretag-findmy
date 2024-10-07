9bdba6bf659b421d03c311a6f561636e4a2f64ea
Ken Su <42598759+KenSu2003@users.noreply.github.com>
2024-10-07 15:17:02 -0400

    updated firmware command

--- firmware.c
+++ firmware.c
@@ -1,3 +1,2 @@
-# For the nrf51
 export LC_CTYPE=C
-xxd -p -c 100000 PREFIX_keyfile | xxd -r -p | dd of=nrf51_firmware.bin skip=1 bs=1 seek=$(grep -oba OFFLINEFINDINGPUBLICKEYHERE! nrf51_firmware.bin | cut -d ':' -f 1) conv=notrunc
+xxd -p -c 100000 4LH8EO_keyfile | xxd -r -p | dd of=nrf51_firmware.bin skip=1 bs=1 seek=$(grep -oba OFFLINEFINDINGPUBLICKEYHERE! nrf51_firmware.bin | cut -d ':' -f 1) conv=notrunc
